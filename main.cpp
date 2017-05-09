264b41585623f7cd23dd4527b39081a7be855e4b
HaydenSolo <haydensolo@gmail.com>
2017-05-09 16:00:01 +1200

    Tidied code
    Put the checking for the line into its own method.

--- main.cpp
+++ main.cpp
@@ -3,31 +3,41 @@
 #include "E101.h"
 #include <limits.h>
 
-int main(){
-	init();
-	while(true) {
-		int sum = 0;
-		int i;
-		int w;
-		int numWhite = 0;
-		take_picture();
-		for(i = 0; i<320; i++) {
-			w = get_pixel(120,i,3);
-			if (w > 128) {
-				w = 1;
-				numWhite++;
-			} else {
-				w = 0;
-			}
-			sum += w*(i-160);
-			
+
+
+int lineCheck() {
+	int sum = 0;
+	int i;
+	int w;
+	int numWhite = 0;
+	take_picture();
+	for(i = 0; i<320; i++) {
+		w = get_pixel(120,i,3);
+		if (w > 128) {
+			w = 1;
+			numWhite++;
+		} else {
+			w = 0;
 		}
-		if(numWhite == 0){
-				sum = INT_MAX;
-			}
-		printf("The sum is %d\n",sum); 
-		sleep1 (0, 500000);
+		sum += w*(i-160);
 		
 	}
+	if(numWhite == 0){
+			sum = INT_MAX;
+	}
 	
-return 0;}
+	return sum;
+}
+
+int main(){
+	init();
+	int error = 0;
+	while(true) {
+		error = lineCheck(); 
+		printf("The error is %d\n",error);
+		sleep1 (0, 500000);	
+	}
+	return 0;
+}
+
+
