bbe734d64b8648ad0056145282805f5c7d1e7c13
HaydenSolo <haydensolo@gmail.com>
2017-05-20 16:54:49 +1200

    Update main.cpp
    Detects when it has reached the line marking the start of quad 3

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@
 #include "E101.h"
 #include <limits.h>
 
+//motor 1 - right wheel
+//motor 2 - left wheel
 
+bool quad3 = false;
 
-int lineCheck() {
+int lineCheck(int line) {
 	int sum = 0;
 	int i;
 	int w;
 	int numWhite = 0;
+	int whiteThresh = 100;
 	take_picture();
 	for(i = 0; i<320; i++) {
-		w = get_pixel(120,i,3);
-		if (w > 80) {
+		w = get_pixel(line,i,3);
+		if (w > whiteThresh) {
 			w = 1;
 			numWhite++;
 		} else {
@@ -23,7 +27,10 @@ int lineCheck() {
 		
 	}
 	if(numWhite == 0){
-			sum = INT_MAX;
+		sum = INT_MAX;
+	}
+	if (numWhite == 320) {
+		quad3 =true;
 	}
 	
 	return sum;
@@ -38,17 +45,35 @@ void runMotors(int error) {
 	} else {
 		set_motor(1,-baseSpeed);
 		set_motor(2,baseSpeed);
+		sleep1(0, 500000);
 	}
 }
 
 int main(){
 	init();
 	int error = 0;
+	int topError = 0;
 	while(true) {
-		error = lineCheck(); 
+		error = lineCheck(120); 
+		runMotors(error);
+		sleep1 (0, 100000);	
+		if (quad3) {
+			break;
+		}
+	}
+	
+	while(true) {
+		error = lineCheck(120); 
+		topError = lineCheck(1); 
+		if(topError == INT_MAX && error != INT_MAX) {
+			set_motor(1,2);
+			set_motor(2,2);
+			break;
+		}
 		runMotors(error);
 		sleep1 (0, 100000);	
 	}
+	
 	return 0;
 }
 
