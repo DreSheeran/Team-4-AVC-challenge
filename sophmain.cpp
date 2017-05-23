0613edb0ee2358ac41fd45fee6c635c9e8c620fc
LeannanSC <sophie.cotterill@gmail.com>
2017-05-23 20:37:23 +1200

    Added Sophie's code
    I fiddled with the main code for quad 3. Turns are a bit awkward but it works most times :P
    Also added a nifty bit of code so the motors won't continue running when we ctrl+c

---
+++ sophmain.cpp
@@ -0,0 +1,134 @@
+#include <stdio.h> 
+#include <time.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include "E101.h" 
+#include <limits.h>
+
+//motor 1 - right wheel
+//motor 2 - left wheel
+
+bool allWhite = false;
+int baseSpeed = 50;
+
+//should stop motors when disconnected
+void signal_callback_handler(int signum){
+	printf("Caught signal %d\n",signum);
+	set_motor(1,0);
+	set_motor(2,0);
+	exit(signum);
+	}
+
+int lineCheck(int line) {
+	int sum = 0;
+	int i;
+	int w;
+	int numWhite = 0;
+	int whiteThresh = 120;
+	take_picture();
+	for(i = 0; i<320; i++) {
+		w = get_pixel(line,i,3);
+		if (w > whiteThresh) {
+			w = 1;
+			numWhite++;
+		} else {
+			w = 0;
+		}
+		sum += w*(i-160);
+
+	}
+	if(numWhite == 0){
+		sum = INT_MAX;
+	}
+	if (numWhite == 320) {
+		allWhite = true;
+	}
+
+	return sum;
+}
+
+void runMotors(int error) {
+	int dimen = 450;
+	if (error != INT_MAX) {
+		set_motor(1,baseSpeed - error/dimen);
+		set_motor(2,-baseSpeed - error/dimen);
+	} else {
+		set_motor(1,-baseSpeed);
+		set_motor(2,baseSpeed);
+		sleep1(0, 300000);
+	}
+}
+
+void turnRight() {
+	int error = INT_MAX;
+	int topError = INT_MAX;
+	int botError = INT_MAX;
+	while(error == INT_MAX && topError == INT_MAX && botError == INT_MAX){
+		set_motor(1,-baseSpeed);
+		set_motor(2,-baseSpeed);
+		error = lineCheck(120);
+		topError = lineCheck(1);
+		botError = lineCheck(239);
+	}
+}
+
+void turnLeft() {
+	int error = INT_MAX;
+	int topError = INT_MAX;
+	int botError = INT_MAX;
+	while(error == INT_MAX && topError == INT_MAX && botError == INT_MAX) {
+		set_motor(1,baseSpeed);
+		set_motor(2,baseSpeed);
+		error = lineCheck(120);
+		topError = lineCheck(1);
+		botError = lineCheck(239);
+	}
+}
+void openGate() {
+	char server[15] = {0};
+	sprintf(server, "130.195.6.196");
+	connect_to_server(server, 1024);
+	char message[24] ={0};
+	sprintf(message, "Please");
+	send_to_server(message);
+	receive_from_server(message);
+	send_to_server(message);
+}
+
+int main(){
+	init();
+	int error = 0;
+	int topError = 0;
+	//int botError = 0;
+	//int lastError = 0;
+	openGate();
+	signal(2,signal_callback_handler);
+	while(true) {
+		error = lineCheck(120);
+		runMotors(error);
+		sleep1 (0, 100000);
+		if (allWhite) {
+			break;
+		}
+	}
+	while(true) {
+		error = lineCheck(120);
+		topError = lineCheck(1);
+		//botError = lineCheck(239);
+		if(topError == INT_MAX) {
+			if(error <= 0) {
+				turnLeft();
+				turnLeft();
+			}else if(error >0) {
+				turnRight();
+				turnRight();
+			}
+		} 
+		else {
+			runMotors(error);
+		}
+		sleep1 (0, 100000);
+	}
+	return 0;
+}
